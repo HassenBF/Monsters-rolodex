171d3dc16305bca80a1677089b1006a74e6282d3
benf.hassen@gmail.com <11031989Hb10!:ghub>
2020-04-22 14:04:34 +0200

    Updates

---
+++ precache-manifest.d856785c92a68dd294e5f9e88e910820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee39d24cdd92c3ab6b9685c51a7fe08",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "722a6055d95e857a6004",
+    "url": "/monsters-rolodex/static/css/main.e329d8e2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722a6055d95e857a6004",
+    "url": "/monsters-rolodex/static/js/main.8c7f7d1b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
